c58c19de0f31c161a827d4d194e6e75852b090e4
Andrew <andrew.sf.liao@gmail.com>
2019-04-11 18:50:20 -0400

    adding preliminary sql statements from Andrew Liao

--- queries.py
+++ queries.py
@@ -8,28 +8,50 @@ GET_TITLE_ISBN = ("SELECT book.title"
 "FROM book "
 "WHERE book.ISBN = %s")
 
-sell_book
+INSERT_USER = ("INSERT INTO user "
+"VALUES(%s, %s, %s")
+
 GET_TITLE_ISBN = ("COUNT(DISTINCT ISBN)"
 "FROM book "
 "WHERE book.ISBN = %s")
 
-
+BUY_BOOK = ("DELETE FROM BOOK "
+"FROM book "
+"WHERE book.ISBN = %s")
 
 INSERT_AUTHOR = ("INSERT INTO author "
 "VALUES(%s, %s)")
 
+INSERT_LISTING =  ("INSERT INTO listing "
+"VALUES(%s, %s, %s, %s, %s, %s, %s)")
 
 MATCH_USER_ORDER_BASKET = ("SELECT order_basket_id"
 "FROM order_basket, user"
 "WHERE order_basket.user_id = uuser.user_id")
 
+MATCH_ORDER_BASKET_LISTING = ("SELECT listing.listing_id"
+"FROM listing, order_basket"
+"WHERE order_basket.order_basket_id = listing.order_order_basket_id")
 
 
-GET_AUTHORNAME_ISBN = (
+
+MATCH_AUTHORNAME_ISBN = (
 "SELECT author.author_name"
 "FROM authors, author"
 "WHERE author.author_id = authors.author_id ^ authors.isbn = %s")
 
+MATCH_PUBLISHER_BOOK = (
+"SELECT book.ISBN"
+"FROM book, publisher"
+"WHERE book.publisher_pub_id = publisher.pub_id"
+)
+
+
+SORT_LOWEST_PRICE = (
+"SELECT listing.listing_id"
+"FROM LISTING"
+"ORDER BY listing.price ASC"
+)
 
 GET_NUMLISTINGS = ("SELECT COUNT(DISTINCT listing_id)"
 "FROM user, listing"
@@ -39,3 +61,5 @@ GET_NUMLISTINGS = ("SELECT COUNT(DISTINCT listing_id)"
 INSERT_AUTHORS = ("INSERT INTO authors"
 "VALUES(%s, %s)")
 
+INSERT_PUBLISHER = ("INSERT INTO publisher "
+"VALUES(%s, %s)")
