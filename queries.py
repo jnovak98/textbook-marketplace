6daa1d7d43c90efbe69a08fa66fb5dc5ff0adc06
BertramSu <40878105+BertramSu@users.noreply.github.com>
2019-04-15 17:05:45 -0400

    Update queries.py
    Fixed #4's name to get_isbncount

--- queries.py
+++ queries.py
@@ -19,7 +19,7 @@ INSERT_USER = ("INSERT INTO user "
 INSERT_BOOK = (“INSERT INTO book”
                “VALUES(%s, %s, %s, %s)")
 #4 get title by isbn
-GET_TITLE_ISBN = ("COUNT(DISTINCT ISBN)"
+GET_ISBNCOUNT = ("COUNT(DISTINCT ISBN)"
 "FROM book "
 "WHERE book.ISBN = %s")
 
