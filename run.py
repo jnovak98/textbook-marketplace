1e309033e1e4b776d5f56f8f25ec0464fe07c64f
jnovak98 <36611520+jnovak98@users.noreply.github.com>
2019-04-17 15:53:21 -0400

    Added book listings page

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import configparser
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 from queries import*
 
@@ -40,9 +40,9 @@ def basic_response():
 @app.route('/')
 def index():
     #we havent decided what these books should be, maybe books with most recently made listings?
-    placeholder_books=[{'title': 'Featured Book Title 1', 'subject': 'Math', 'description': 'This is a placeholder'},
-        {'title': 'Featured Book Title 2', 'subject': 'Physics', 'description': 'This is also a placeholder'},
-        {'title': 'Featured Book Title 3', 'subject': 'English', 'description': 'Another placeholder'}]
+    placeholder_books=[{'title': 'Featured Book Title 1', 'subject': 'Math', 'description': 'This is a placeholder','isbn':789789789, 'authors':[{'author_name': 'Author'}]},
+        {'title': 'Featured Book Title 2', 'subject': 'Physics', 'description': 'This is also a placeholder','isbn':123123123, 'authors':[{'author_name': 'Author'}]},
+        {'title': 'Featured Book Title 3', 'subject': 'English', 'description': 'Another placeholder','isbn':456456456, 'authors':[{'author_name': 'Author'}]}]
     return render_template('home.html', books=placeholder_books)
 
 @app.route('/search')
@@ -51,13 +51,33 @@ def search():
         search_query=request.args.get('query')
         #these books should be every book in the DB that contains "search_query", 
         #either in the name or description, based on how much it shows up
-        placeholder_books=[{'title': 'Book Search Result 1', 'subject': 'Math', 'description': 'This is a placeholder search result'},
-        {'title': 'Book Search Result 2', 'subject': 'Physics', 'description': 'Same'},
-        {'title': 'Book Search Result 3', 'subject': 'English', 'description': 'yeet'},
-        {'title': 'Book Search Result 4', 'subject': 'a subject', 'description': 'a description'},]
+        placeholder_books=[{'title': 'Book Search Result 1', 'subject': 'Math', 'description': 'This is a placeholder search result','isbn':123456, 'authors':[{'author_name': 'Author'}]},
+        {'title': 'Book Search Result 2', 'subject': 'Physics', 'description': 'Same','isbn':123123123 , 'authors':[{'author_name': 'Author'}]},
+        {'title': 'Book Search Result 3', 'subject': 'English', 'description': 'yeet','isbn':789789789, 'authors':[{'author_name': 'Author'}]},
+        {'title': 'Book Search Result 4', 'subject': 'a subject', 'description': 'a description','isbn':456456456, 'authors':[{'author_name': 'Author'}]}]
         return render_template('search.html', books=placeholder_books, query=search_query)
     else:
         return redirect(url_for('index'))
+
+@app.route('/book-listings/<int:isbn>', methods=('GET', 'POST'))
+def book_listings(isbn):
+    if request.method == 'GET':
+        #get book based on 'isbn'. Make sure this includes an array of all the authors
+        book_details={'title': 'Placeholder Book', 'subject': 'Math', 'description': 'This is a placeholder book listings page', 
+            'isbn': isbn, 'authors':[{'author_name': 'Author 1'}, {'author_name': 'Author 2'}]}
+        # listings of 'isbn' that are available. Make sure this includes the username of the user who made the listing
+        listings=[{'listing_id':123123123,'price': '$20', 'listing_condition': 'new', 'username': 'user1'},
+        {'listing_id':456456456,'price': '$10', 'listing_condition': 'Used - Fair','username': 'user2'},
+        {'listing_id':789789789,'price': '$15', 'listing_condition': 'Used - Like New','username': 'user3'}]
+        #User's unordered baskets 
+        baskets=[{'order_basket_id':149872},{'order_basket_id': 476343},{'order_basket_id':345645}]
+        return render_template('book-listings.html', book=book_details, listings=listings, baskets=baskets)
+    if request.method == 'POST':
+        listing_id = request.form['listing_id']
+        order_basket_id = request.form['order_basket_id']
+        #add listing_id to order basket with id=order_basket_id
+        return redirect(url_for('book_listings', isbn=isbn)), 200
+
 
 #@app.route('/', methods=['GET', 'POST'])
 def template_response_with_data():
