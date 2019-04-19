07229d71d718d310cd2e71e30276c001ab707eef
Andrew <andrew.sf.liao@gmail.com>
2019-04-19 18:00:00 -0400

    adding params

--- run.py
+++ run.py
@@ -12,20 +12,20 @@ config.read('config.ini')
 app = Flask(__name__)
 
 # Create a function for fetching data from the database.
-def sql_query(sql):
+def sql_query(sql, params = None):
     db = mysql.connector.connect(**config['mysql.connector'])
     cursor = db.cursor()
-    cursor.execute(sql)
+    cursor.execute(sql, params)
     result = cursor.fetchall()
     cursor.close()
     db.close()
     return result
 
 
-def sql_execute(sql):
+def sql_execute(sql, params):
     db = mysql.connector.connect(**config['mysql.connector'])
     cursor = db.cursor()
-    cursor.execute(sql)
+    cursor.execute(sql, params)
     db.commit()
     cursor.close()
     db.close()
@@ -102,7 +102,7 @@ def add_book(isbn):
         authors = request.form['authors'] #names seperated by commas. Might need to make new author if author with that name doesn't already exist
         publisher = request.form['publisher'] #might have to make new publisher if publisher with this name doesn't exist
         description = request.form['description']
-        sql_query(INSERT_BOOK, isbn, subject, title, description, 100)
+        sql_query(INSERT_BOOK, params=(isbn, subject, title, description, 100))
         #add this new book to DB
         return redirect(url_for('book_listings', isbn=isbn))
 
@@ -199,8 +199,7 @@ def insertPublisher(pub_id, pub_name):
 
 #3
 def insertBook(isbn, subject, title, description, pub_id):
-
-    #return sql_query(INSERT_BOOK, isbn, subject, title, description, pub_id)
+    return sql_query(INSERT_BOOK, isbn, subject, title, description, pub_id)
 #9
 def insertOrderBasket(order_basket_id, user_id, address, date_made, order_basket_status):
     return sql_query(INSERT_ORDER_BASKET, order_basket_id, user_id, address, date_made, order_basket_status)
