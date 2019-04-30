efef1250a8b031a46602e505cfa55327077cc47f
BertramSu <40878105+BertramSu@users.noreply.github.com>
2019-04-30 13:52:37 -0400

    Update run.py

--- run.py
+++ run.py
@@ -127,10 +127,10 @@ def book_listings(isbn):
         title = book_result[0][0].decode("utf-8")
         subject = book_result[0][1].decode("utf-8")
         description = book_result[0][2].decode("utf-8")
-        author = [0][3].decode("utf-8")
+        author = [0][4].decode("utf-8")
 
         book_details={'title': title, 'subject': subject, 'description': description,
-            'isbn': isbn, 'authors': author}
+            'isbn': isbn, 'author': author}
         # listings of 'isbn' that are available. Make sure this includes the username of the user who made the listing
 
         listing_search = sql_query(GET_LISTING_BOOK_ISBN, params=(isbn,))
